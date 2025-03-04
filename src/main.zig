42e6d9f6eaf089b6416a912548f77301e516a296
Chaituvarma-109 <chaitanyavarma.u@gmail.com>
2025-03-04 12:44:22 +0530

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -11,6 +11,17 @@ pub fn main() !void {
         var buffer: [1024]u8 = undefined;
         const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
 
-        try stdout.print("{s}: command not found\n", .{user_input});
+        const trim_inp = std.mem.trim(u8, user_input, "\r\n");
+        var token_iter = std.mem.tokenizeAny(u8, trim_inp, " ");
+
+        const cmd = token_iter.next();
+
+        if (cmd) |c| {
+            if (std.mem.eql(u8, c, "exit")) {
+                std.posix.exit(0);
+            } else {
+                try stdout.print("{s}: command not found\n", .{c});
+            }
+        }
     }
 }
